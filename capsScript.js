7df147ff0b05cfe5a0fa18b4a1dd1bd7660f6036
noddycode <neorescuer@gmail.com>
2017-03-17 08:11:19 -0400

    Committed for final testing

--- capsScript.js
+++ capsScript.js
@@ -1,5 +1,23 @@
-
-var randomize = function(string)
+var randomizer = function(char)
 {
-	var cap = Math.random() >- 0.5;
+	var caps = Math.random() > 0.5;
+
+	if(caps)
+		return char.toUpperCase();
+	else
+		return char.toLowerCase();
 }
+
+var randomize = function()
+{
+	var input = document.getElementById('string-input').value;
+	var cap = Math.random() >- 0.5;
+	
+	var output = input.replace(/[<>]/g, '');
+
+	output = output.replace(/[A-Za-z]/gi, randomizer);
+
+	document.getElementById('output-textarea').value = output;
+	document.getElementById('output').style.display = "block";
+}
+
